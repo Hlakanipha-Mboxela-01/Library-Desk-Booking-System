c3f31a39d8f660df1717ee79b280c5d434245654
Hlakanipha-Mboxela-01 <215057627@mycput.ac.za>
2025-04-20 18:43:54 +0200

    SimpleFactoryTest.java under tests directory

--- tests/SimpleFactoryTest.java
+++ tests/SimpleFactoryTest.java
@@ -1,1 +1,15 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class SimpleFactoryTest {
+
+    @Test
+    public void testCreateDesk() {
+        Desk studyDesk = DeskFactory.createDesk("study");
+        assertEquals("Study Desk", studyDesk.getType());
+
+        Desk compDesk = DeskFactory.createDesk("computer");
+        assertEquals("Computer Desk", compDesk.getType());
+    }
+}
 
