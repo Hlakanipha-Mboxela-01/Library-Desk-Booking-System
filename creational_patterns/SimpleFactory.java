4938514c6247a7b40bba4e059587639c075d6076
Hlakanipha-Mboxela-01 <215057627@mycput.ac.za>
2025-04-20 16:13:20 +0200

    Added SimpleFactory pattern implementation.

---
+++ creational_patterns/SimpleFactory.java
@@ -0,0 +1,38 @@
+// SimpleFactory.java
+
+public class VehicleFactory {
+    public static Vehicle createVehicle(String type) {
+        switch (type.toLowerCase()) {
+            case "car":
+                return new Car();
+            case "bike":
+                return new Bike();
+            case "truck":
+                return new Truck();
+            default:
+                throw new IllegalArgumentException("Unknown vehicle type");
+        }
+    }
+}
+
+interface Vehicle {
+    void drive();
+}
+
+class Car implements Vehicle {
+    public void drive() {
+        System.out.println("Driving a car");
+    }
+}
+
+class Bike implements Vehicle {
+    public void drive() {
+        System.out.println("Riding a bike");
+    }
+}
+
+class Truck implements Vehicle {
+    public void drive() {
+        System.out.println("Driving a truck");
+    }
+}
