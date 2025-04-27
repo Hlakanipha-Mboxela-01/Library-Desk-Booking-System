e817d8a4ef1e9461f99fcf8ea0547d97fc495b80
Hlakanipha-Mboxela-01 <215057627@mycput.ac.za>
2025-04-27 20:52:29 +0200

    Update Repository.java

--- src/repositories/Repository.java
+++ src/repositories/Repository.java
@@ -1,1 +1,12 @@
+package repositories;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface Repository<T, ID> {
+    void save(T entity);           // Create or Update
+    Optional<T> findById(ID id);    // Read one
+    List<T> findAll();              // Read all
+    void delete(ID id);             // Delete
+}
 
