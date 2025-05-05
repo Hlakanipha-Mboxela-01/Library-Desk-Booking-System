8939fbb12e1bf805369282962c9d097ef7316a38
Hlakanipha-Mboxela-01 <215057627@mycput.ac.za>
2025-05-05 04:13:29 +0200

    Create Testing BookController with JUnit & MockMvc.java

---
+++ src/main/java/api/Testing BookController with JUnit & MockMvc.java
@@ -0,0 +1,37 @@
+package api;
+
+import models.Book;
+import services.BookService;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import java.util.List;
+
+@WebMvcTest(BookController.class)
+public class BookControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private BookService bookService;
+
+    @Test
+    public void testGetAllBooks() throws Exception {
+        Book book = new Book("1", "Clean Code", "Robert Martin");
+        Mockito.when(bookService.getAllBooks()).thenReturn(List.of(book));
+
+        mockMvc.perform(get("/api/books"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].title").value("Clean Code"));
+    }
+}
